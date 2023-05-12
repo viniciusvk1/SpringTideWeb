86e4e6bef8eb506c3b1815b1f41bff481e024b92
Vinicius Almeida <viniciusalmeida.vk1@gmail.com>
2023-05-12 16:13:23 -0300

    add a UserRepository and crud methods

---
+++ src/main/java/br/com/api/ApiProject/DAO/UserRepository.java
@@ -0,0 +1,71 @@
+package br.com.api.ApiProject.DAO;
+
+import br.com.api.ApiProject.Model.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public class UserRepository implements CrudRepository<User, Integer> {
+
+    @Override
+    public <S extends User> S save(S entity) {
+        return null;
+    }
+
+    @Override
+    public <S extends User> Iterable<S> saveAll(Iterable<S> entities) {
+        return null;
+    }
+
+    @Override
+    public Optional<User> findById(Integer integer) {
+        return Optional.empty();
+    }
+
+    @Override
+    public boolean existsById(Integer integer) {
+        return false;
+    }
+
+    @Override
+    public Iterable<User> findAll() {
+        return null;
+    }
+
+    @Override
+    public Iterable<User> findAllById(Iterable<Integer> integers) {
+        return null;
+    }
+
+    @Override
+    public long count() {
+        return 0;
+    }
+
+    @Override
+    public void deleteById(Integer integer) {
+
+    }
+
+    @Override
+    public void delete(User entity) {
+
+    }
+
+    @Override
+    public void deleteAllById(Iterable<? extends Integer> integers) {
+
+    }
+
+    @Override
+    public void deleteAll(Iterable<? extends User> entities) {
+
+    }
+
+    @Override
+    public void deleteAll() {
+
+    }
+}
--- src/main/java/br/com/api/ApiProject/controller/UserController.java
+++ src/main/java/br/com/api/ApiProject/controller/UserController.java
@@ -1,13 +1,23 @@
 package br.com.api.ApiProject.controller;
 
+import br.com.api.ApiProject.DAO.UserRepository;
+import br.com.api.ApiProject.Model.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
-public class UserController {
+public class UserController{
+
+    @Autowired
+    private UserRepository dao;
 
     @GetMapping("/user")
-    public String text() {
-        return "Acessing the api";
+    public List<User> usersList() {
+        return (List<User>) dao.findAll();
     }
+
+
 }
