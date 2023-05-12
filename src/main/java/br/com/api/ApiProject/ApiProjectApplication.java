896d6b18ccc1d61e048e84ae67db90b91479cb83
Vinicius Almeida <viniciusalmeida.vk1@gmail.com>
2023-05-12 14:15:27 -0300

    initial files

---
+++ src/main/java/br/com/api/ApiProject/ApiProjectApplication.java
@@ -0,0 +1,13 @@
+package br.com.api.ApiProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/api/ApiProject/ApiProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.api.ApiProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
